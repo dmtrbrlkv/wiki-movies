a3b42fbded092d80ea2a46f38a02c8cc02ac36b4
Dmitry Burlakov <dburlakov@mtsbank.ru>
2024-01-26 16:13:35 +0300

    Настройки для парсинга рейтинга

--- wiki_movies_scrapy/wiki_movies_scrapy/settings.py
+++ wiki_movies_scrapy/wiki_movies_scrapy/settings.py
@@ -91,3 +91,7 @@ ROBOTSTXT_OBEY = False
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+PARSE_RATING = False
+PROXIES = {'https': ''}
+API_KEY = ''
